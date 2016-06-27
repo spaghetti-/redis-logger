b3f608d9aaa87a593e8bcda3781a69a641e9b6cd
Alex <dev@xrng.net>
2016-06-27 18:12:50 +0800

    Optional, iterable transports

--- redis-logger.js
+++ redis-logger.js
@@ -1,6 +1,7 @@
 var redis = require('redis');
 var stackTrace = require('stack-trace');
 var colors = require('colors');
+var util = require('util');
 
 var Logger = exports.Logger = function(options) {
   if (options.redis instanceof redis.RedisClient) {
@@ -15,11 +16,41 @@ var Logger = exports.Logger = function(options) {
   this.timestamp = options.timestamp || false;
   this.key = options.key || 'logstash:debug';
   this.console = options.console || false;
+
+  this.transports = {
+    'console': this.consoleTransport,
+    'redis': this.redisTransport
+  }
+
   this.defaultLogObject = {
     hostname: require('os').hostname(),
     service: options.service || "redis-logger",
     level: "INFO"
   };
+}
+
+Logger.prototype.redisTransport = function (t, o) {
+  t.client.lpush(t.key, JSON.stringify(o));
+}
+
+Logger.prototype.consoleTransport = function (t, o) {
+  if (!t.console) return;
+  var errorString = util.format('%s [%s:%s %s]: %s\n',
+                                o.level === 'FATAL' ? o.level.red :
+                                o.level === 'WARN' ? o.level.yellow :
+                                o.level,
+                                o.filename,
+                                o.linenumber,
+                                o.method,
+                                o.message);
+  if (o.level === 'FATAL') process.stderr.write(errorString);
+  else process.stdout.write(errorString);
+}
+
+Logger.prototype.send = function (o) {
+  for (t in this.transports) {
+    this.transports[t](this, o);
+  }
 }
 
 Logger.prototype.getDebugInfo = function() {
@@ -35,25 +66,19 @@ Logger.prototype.getDebugInfo = function() {
 Logger.prototype.info = function (message) {
   var Log = this.getDebugInfo();
   Log.message = message;
-  if (this.console)
-    console.log('INFO' + ': ' + message);
-  this.client.lpush(this.key, JSON.stringify(Log));
+  this.send(Log);
 }
 
 Logger.prototype.warn = function (message) {
   var Log = this.getDebugInfo();
   Log.level = "WARN";
   Log.message = message;
-  if (this.console)
-    console.log('WARN'.yellow + ': ' + message);
-  this.client.lpush(this.key, JSON.stringify(Log));
+  this.send(Log);
 }
 
 Logger.prototype.error = Logger.prototype.fatal = function (message) {
   var Log = this.getDebugInfo();
   Log.level = "FATAL";
   Log.message = message;
-  if (this.console)
-    console.log('ERROR'.red + ': ' + message);
-  this.client.lpush(this.key, JSON.stringify(Log));
+  this.send(Log);
 }
